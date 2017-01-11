388b2227f02baa55abdcfc4c5a3f3f28487b8a93
HAOYU WU <Haoyu@MyMac.local>
2017-01-10 23:58:22 -0800

    MisMatch_First commit

---
+++ mismatch/connectvars.php
@@ -0,0 +1,7 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', '127.0.0.1');
+  define('DB_USER', 'mismatch_user');
+  define('DB_PASSWORD', 'mismatch_1_pass');
+  define('DB_NAME', 'mismatch_1');
+?>
